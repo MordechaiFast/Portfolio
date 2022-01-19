a664169cc4ae1426d9c5b02b40b1e83b3d56999a
Mordechai Fast <mordechai.fast@gmail.com>
2022-01-19 15:42:19 +0200

    init db

--- projects/models.py
+++ projects/models.py
@@ -1,3 +1,8 @@
+from turtle import title
 from django.db import models
 
-# Create your models here.
+class Project(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    technology = models.CharField(max_length=30)
+    link = models.FilePathField(allow_folders=True)
