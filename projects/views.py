af5fd1a4cc650c2075c08b3e8fb5face48a231e3
Mordechai Fast <mordechai.fast@gmail.com>
2022-01-20 22:47:06 +0200

    set up project page near compleate

--- projects/views.py
+++ projects/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from projects.models import Project
 
-# Create your views here.
+def project_index(request):
+    context = {'projects': Project.objects.all()}
+    return render(request, 'project_index.html', context)
+
+""" With the projects that I have here, a demo is more appropiate than a descriptive page. This is beyond my current abillity.
+def project_detail(request, pk):
+    context = {'project': Project.objects.get(pk=pk)}
+    return render(request, 'project_detail.html', context) """
