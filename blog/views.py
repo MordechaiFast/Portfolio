89bb6170aefe1902d04211896b2b374bab6c6d6c
Mordechai Fast <mordechai.fast@gmail.com>
2022-01-21 02:45:44 +0200

    Added blog

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from blog.models import Post, Comment
+from .forms import CommentForm
 
 def blog_index(request):
     context = {'posts': Post.objects.all().order_by('-created_on')}
@@ -14,5 +15,15 @@ def blog_category(request, category):
 def blog_detail(request, pk):
     post = Post.objects.get(pk=pk)
     comments = Comment.objects.filter(post=post)
+    form = CommentForm()
+    if request.method == 'POST':
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            comment = Comment(
+                author=form.cleaned_data["author"],
+                body=form.cleaned_data["body"],
+                post=post
+            )
+            comment.save()
     context = {'post': post, 'comments': comments}
     return render(request, 'blog_detail.html', context)
